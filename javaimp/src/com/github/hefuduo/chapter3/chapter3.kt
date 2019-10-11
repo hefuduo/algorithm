e8726a544d9bb577bd5574e2f0f05a978379e549
何富多 <hefuduo@meituan.com>
2019-10-11 19:40:52 +0800

    cause:chapter 3 solution:done
    Signed-off-by: 何富多 <hefuduo@meituan.com>

---
+++ javaimp/src/com/github/hefuduo/chapter3/chapter3.kt
@@ -0,0 +1,19 @@
+package com.github.hefuduo.chapter3
+
+/**com.github.hefuduo.chapter3
+ * Created by hefuduo on 2019-10-11.
+ */
+
+/**
+ * 3.1
+ * l 和 p 是有序链表, 输出l中p存储的index
+ * @param l List<Int>
+ * @param p List<Int>
+ */
+fun printLots(l: List<Int>, p: List<Int>) {
+    p.forEach{
+        println(l[it])
+    }
+}
+
+
--- javaimp/src/com/github/hefuduo/main.kt
+++ javaimp/src/com/github/hefuduo/main.kt
@@ -1,8 +1,12 @@
 package com.github.hefuduo
 
+import com.github.hefuduo.chapter3.printLots
+
 /**
  * Created by hefuduo on 2019-09-25.
  */
 fun main() {
-    print("hefuduo")
+    val l = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val p = listOf(1, 3, 4, 6)
+    printLots(l, p)
 }
